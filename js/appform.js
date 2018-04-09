528b432584096f7c0fa6eda06bdc381e74caa960
Julien <julien.chenel@gmail.com>
2018-04-09 15:54:10 +0200

    placement boutons ok

--- js/appform.js
+++ js/appform.js
@@ -1,15 +1,12 @@
-var btCamera = $('#cadrePhoto').html();
-
 function handleFiles(files) {
     for (var i = 0; i < files.length; i++) {
-        console.log("files.length")
+        console.log(files.length)
         var file = files[i];
         var imageType = /^image\//;
 
         if (!imageType.test(file.type)) {
             continue;
         }
-        console.log(btCamera);
         var img = document.createElement("img");
         img.classList.add("miniature");
         img.file = file;
@@ -19,9 +16,9 @@ function handleFiles(files) {
         reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
         reader.readAsDataURL(file);
 
-        $('#cadrePhoto').remove();
-        $('#supprimer').html("<label class='suppButton mx-auto'><i class='fas fa-times-circle fa-3x mx-auto'></i><input type='button' onchange='removePhoto()'>");
-        $("#supprimer").show()
+        $('#cadrePhoto').children().hide();
+        $('#supprimer').html("<label class='suppButton'><i class='fas fa-times-circle fa-3x'></i><input type='button' onchange='removePhoto()'>");
+        $("#supprimer").show();
 
     }
 }
@@ -29,7 +26,8 @@ function handleFiles(files) {
 $('#supprimer').on("click", removePhoto);
 
 function removePhoto() {
-    $(".miniature").remove();
-    $("#supprimer").hide();
-    $("#cadrePhoto2").html(btCamera);
+    // Une même image ne s'affichera pas si elle est rechargée une seconde fois.
+    $("#preview").children().remove();
+    $("#supprimer").children().hide();
+    $('#cadrePhoto').children().show();
 }
