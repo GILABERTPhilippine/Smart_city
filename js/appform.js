8a3a1afedd1c3b405677dc52a3fc87f4d3343bfd
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-04-11 17:40:33 +0200

    modifs localstorage a tester

--- js/appform.js
+++ js/appform.js
@@ -2,157 +2,101 @@
 
     
     function handleFiles(files) {
-    for (var i = 0; i < files.length; i++) {
-        console.log(files.length)
-        var file = files[i];
-        var imageType = /^image\//;
-
-        if (!imageType.test(file.type)) {
-            continue;
+        for (var i = 0; i < files.length; i++) {
+            console.log(files.length)
+            var file = files[i];
+            var imageType = /^image\//;
+    
+            if (!imageType.test(file.type)) {
+                continue;
+            }
+            var img = document.createElement("img");
+            img.classList.add("miniature");
+            img.file = file;
+            document.getElementById("preview").appendChild(img); // En admettant que "preview" est l'élément div qui contiendra le contenu affiché. 
+    
+            var reader = new FileReader();
+            reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+            reader.readAsDataURL(file);
+    
+            $('#cadrePhoto').children().hide();
+            $('#supprimer').html("<label class='suppButton'><i class='fas fa-times-circle fa-3x'></i><input type='button' onchange='removePhoto()'>");
+            $("#supprimer").show();
+    
         }
-        var img = document.createElement("img");
-        img.classList.add("miniature");
-        img.file = file;
-        document.getElementById("preview").appendChild(img); // En admettant que "preview" est l'élément div qui contiendra le contenu affiché. 
-
-        var reader = new FileReader();
-        reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-        reader.readAsDataURL(file);
-
-        $('#cadrePhoto').children().hide();
-        $('#supprimer').html("<label class='suppButton'><i class='fas fa-times-circle fa-3x'></i><input type='button' onchange='removePhoto()'>");
-        $("#supprimer").show();
-
     }
-}
-
-$('#supprimer').on("click", removePhoto);
-
-function removePhoto() {
-    // Une même image ne s'affichera pas si elle est rechargée une seconde fois.
-    $("#preview").children().remove();
-    $("#supprimer").children().hide();
-    $('#cadrePhoto').children().show();
-}
-
-// $(document).ready(function(){
-//Récupération de la catégorie depuis le sessionstorage
-
-var cat = sessionStorage.getItem("categorie");
-console.log(cat);
-$("#title").text(cat);
-
-var lat = sessionStorage.getItem("lat");
-console.log(lat);
-
-var lon = sessionStorage.getItem("lon");
-console.log(lon);
-
-
-//Stocker le point gps dans un objet userMarkers
-
-var userMarkers = {
-    'obj1':{'lat' : "43.1", 'lon' : "1.6", 'cat' : "1"},
-    'obj2':{'lat' : '43.2', 'lon' : '1.6', 'cat' : '1'},
-    'obj3':{'lat' : 43.3, 'lon' : 1.6, 'cat' : 1},
-    'obj4':{'lat' : 43.4, 'lon' : 1.6, 'cat' : 1},
-    'obj5':{'lat' : 43.5, 'lon' : 1.6, 'cat' : 1},
-    };
-
-
-//Get the size of an object
-Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
+    
+    $('#supprimer').on("click", removePhoto);
+    
+    function removePhoto() {
+        // Une même image ne s'affichera pas si elle est rechargée une seconde fois.
+        $("#preview").children().remove();
+        $("#supprimer").children().hide();
+        $('#cadrePhoto').children().show();
     }
-    return size;
-};
+    
+    $(document).ready(function(){
+    //Récupération de la catégorie depuis le sessionstorage
+    
+    var cat = sessionStorage.getItem("categorie");
+    console.log(cat);
+    $("#title").text(cat);
+    
+    var lat = sessionStorage.getItem("lat");
+    console.log(lat);
+    
+    var lon = sessionStorage.getItem("lon");
+    console.log(lon);
 
-var objSize = Object.size(userMarkers);
+            //Obtenir le nombre d'enregistrements
+            Object.size = function(obj) {
+                var size = 0, key;
+                for (key in obj) {
+                    if (obj.hasOwnProperty(key)) size++;
+                }
+                return size;
+            };
+    
+    //Obtenir les nouvelles valeurs dans le tableau (objet)
+    // var getUserMarkers = JSON.parse(localStorage.getItem("userMarkers"));  
+    
+    // var userMarkers = {
+    //     'obj1':{'lat' : "43.1", 'lon' : "1.6, 'cat' : 1},
+    //     'obj2':{'lat' : 43.2, 'lon' : 1.6, 'cat' : 1},
+    //     'obj3':{'lat' : 43.3, 'lon' : 1.6, 'cat' : 1},
+    //     'obj4':{'lat' : 43.4, 'lon' : 1.6, 'cat' : 1},
+    //     'obj5':{'lat' : 43.5, 'lon' : 1.6, 'cat' : 1},
+    //     };
+    
+    if (!localStorage.getItem("userMarkers")){
+        //Initialiser l'objet dans le localstorage
+        var userMarkers = {'obj1':{'lat':lat,'lon':lon,'cat':cat}};
+        localStorage.setItem("userMarkers",userMarkers);
+        console.log("getUserMarkers : ",getUserMarkers );
+    }else{
 
-console.log("size : ",Object.size(userMarkers));
+    var getUserMarkers = JSON.parse(localStorage.getItem("userMarkers"));
+    console.log("getUserMarkers : ",getUserMarkers );
 
-var userMarkersKey = Object.keys(userMarkers);
-// console.log(userMarkersKey);
-var userMarkersValue = Object.values(userMarkers);
-// console.log(userMarkersValue);
-
-
-// Itération sur clés/valeurs de l'objet et contruction des coordonnnées gps
-
-for(var i = 0; i <= objSize-1;i++){
-    var key = userMarkersKey[i];
-    var values = userMarkersValue[i];
-    console.log("clé", key);
-    console.log("valeurs", values);
-    console.log("latitude",values.lat);
-    console.log("longitude",values.lon);
-    console.log("categorie",values.cat);
-    var latitude = values.lat;
-    var longitude = values.lon;
-// var marker = L.marker([latitude, longitude]);
-
-
-
-}
-
-// console.log("objet",userMarkers.obj1.lat);
-
-
-localStorage.setItem("userMarkers", JSON.stringify(userMarkers));
-
-var getUserMarkers = JSON.parse(localStorage.getItem("userMarkers"));
-
-console.log("getUserMarkers",getUserMarkers);
-
-//ecrire les nouvelles valeurs dans le tableau (objet)
-
-var userMarkers = JSON.parse(localStorage.getItem("userMarkers"));
-
-// ajouter la nouvelle ligne au tableau
-var objetNumber = objSize+1;
-var objet = 'obj'+objetNumber;
-console.log("objet:",objet);
-newEntry = {'lat' :lat,'lon':lon,'cat' :cat};
-console.log(newEntry);
-userMarkers[objet]= newEntry;
-
-//Stocker a nouveau le tableau dans var userMarkers
-localStorage.setItem("userMarkers", JSON.stringify(userMarkers));
-
-var getUserMarkers = JSON.parse(localStorage.getItem("userMarkers"));
-
-console.log("getUserMarkers + newEntry",getUserMarkers);//Regler pb affichage dans l'objet en sortie
-
-//###############################tests#######################
-//Créer les marqueurs
-//Utiliser boucle for et ajouter les valeurs aux marqueurs avec :
-// var latitude = userMarkers.Key.lat;
-// var longitude = userMarkers.Key.lon;
-// var marker = L.marker([latitude, longitude]);
-// console.log(marker);
-
-
-var userLat =[43];
-var userLon =[43];
-
-
-localStorage.setItem("userLat", JSON.stringify(userLat));
-localStorage.setItem("userLon", JSON.stringify(userLon));
-
-var storedLat = JSON.parse(localStorage.getItem("userLat"));
-var storedLon = JSON.parse(localStorage.getItem("userLon"));
-
-console.log("storedLat :",storedLat);
-console.log("storedLon :",storedLon);
-
-latitude = storedLat[0];
-longitude = storedLon[0];
-
-console.log("latitude :",latitude);
-console.log("longitude :",longitude);
-
-
-
-// });
+    
+    //Nombre d'enregistrements dans l'objet
+    var objSize = Object.size(getUserMarkers);
+    
+    console.log("size : ",Object.size(getUserMarkers));
+    
+    // ajouter la nouvelle ligne au tableau
+    var objetNumber = objSize+1;
+    var objet = 'obj'+objetNumber;
+    console.log("objet:",objet);
+    newEntry = {'lat' :lat,'lon':lon,'cat' :cat};
+    console.log(newEntry);
+    getUserMarkers[objet]= newEntry;
+    
+    //Stocker a nouveau le tableau dans var userMarkers
+    setUserMarkers = localStorage.setItem("userMarkers", JSON.stringify(getUserMarkers));
+    }
+    
+    console.log("getUserMarkers",getUserMarkers);
+    
+    
+    });
--- js/map.js
+++ js/map.js
@@ -1,106 +1,160 @@
-$(document).ready(function () {
-    var map;
+// $(document).ready(function () {
+var map;
 
-    function init() {
-        map = new L.Map('map');
-        L.tileLayer('https://api.mapbox.com/styles/v1/julienchenel/cjfdz2p2fa5g92sq0dzgxf95o/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianVsaWVuY2hlbmVsIiwiYSI6ImNqZmR6MGhvYjJiOGo0YXFoejFobXJqaGIifQ.SZ2HjrSNVhc7hCyXZlDv9A', {
-            attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-            maxZoom: 18
-        }).addTo(map);
-        map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
+function init() {
+    map = new L.Map('map');
+    L.tileLayer('https://api.mapbox.com/styles/v1/julienchenel/cjfdz2p2fa5g92sq0dzgxf95o/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianVsaWVuY2hlbmVsIiwiYSI6ImNqZmR6MGhvYjJiOGo0YXFoejFobXJqaGIifQ.SZ2HjrSNVhc7hCyXZlDv9A', {
+        attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+        maxZoom: 18
+    }).addTo(map);
+    map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
 
-        // map view before we get the location
-        map.setView(new L.LatLng(43.116312, 1.612136), 13);
-    }
+    // map view before we get the location
+    map.setView(new L.LatLng(43.116312, 1.612136), 13);
+}
 
-    init();
-    getLocationLeaflet();
+init();
+getLocationLeaflet();
 
-    function onLocationFound(e) {
-        var radius = e.accuracy / 2;
-        var location = e.latlng
-        L.marker(location).addTo(map)
-        L.circle(location, radius).addTo(map);
-    }
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+    var location = e.latlng
+    L.marker(location).addTo(map)
+    L.circle(location, radius).addTo(map);
+}
 
-    function onLocationError(e) {
-        alert(e.message);
-    }
+function onLocationError(e) {
+    alert(e.message);
+}
 
-    function getLocationLeaflet() {
-        map.on('locationfound', onLocationFound);
-        map.on('locationerror', onLocationError);
+function getLocationLeaflet() {
+    map.on('locationfound', onLocationFound);
+    map.on('locationerror', onLocationError);
 
-        map.locate({
-            setView: true,
-            maxZoom: 16
-        });
-    }
+    map.locate({
+        setView: true,
+        maxZoom: 16
+    });
+}
 
-    // Ajouter un marker manuelle et un pop attribuer à ce marker
-    // var marker = L.marker([43.11, 1.61]).addTo(map);
-    // marker.bindPopup("<b>Etat résolu</b>").openPopup();
-
-
-    //Récupération des points gps entrés par l'utilisateur depuis le localstorage
-    var getUserMarkers = JSON.parse(localStorage.getItem("userMarkers"));
-    console.log(getUserMarkers);
+//Récupération des points gps entrés par l'utilisateur depuis le localstorage
+var getUserMarkers = JSON.parse(localStorage.getItem("userMarkers"));
 
     //Obtenir la taille d'un objet
-    Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
+    Object.size = function (obj) {
+        var size = 0,
+            key;
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) size++;
+        }
+        return size;
     };
     
-    //Obtenir le nombre de paires clés/valeurs stockées dans l'objet getUserMarkers
-    var objSize = Object.size(getUserMarkers);
-    console.log("size : ",Object.size(getUserMarkers));
+// console.log(getUserMarkers);
 
-    var userMarkersKey = Object.keys(getUserMarkers);
-    console.log(userMarkersKey);
-    var userMarkersValue = Object.values(getUserMarkers);
-    console.log(userMarkersValue);
+//Si le localstorage n'est pas vide
+if (getUserMarkers) {
 
-    //Itération sur clés/valeurs de l'objet et contruction des coordonnnées gps
-    //Limite le nombre d'itération au nombre de clés/valeurs déjà stockées via objSize
+//Obtenir le nombre de paires clés/valeurs stockées dans l'objet getUserMarkers
+var objSize = Object.size(getUserMarkers);
+// console.log("size : ",Object.size(getUserMarkers));
 
-  
+var userMarkersKey = Object.keys(getUserMarkers);
+// console.log(userMarkersKey);
+var userMarkersValue = Object.values(getUserMarkers);
+// console.log(userMarkersValue);
 
-function createMarkers(){
-  //Création du tableau contenant les différentes coordonnées utilisateur
-  var markers = [];
+}
 
-    for(var i = 0; i <= objSize-1;i++){
+
+
+//Itération sur clés/valeurs de l'objet et contruction des coordonnnées gps
+//Limite le nombre d'itération au nombre de clés/valeurs déjà stockées via objSize
+
+function createMarkers() {
+
+    for (var i = 0; i <= objSize - 1; i++) {
         //récupération de la clé à chaque itération
         var key = userMarkersKey[i];
         //récupération de la valeur à chaque itération
         var values = userMarkersValue[i];
-        console.log("clé", key);
-        console.log("valeurs", values);
-        console.log("latitude",values.lat);
-        console.log("longitude",values.lon);
-        console.log("categorie",values.cat);
+        // console.log("clé", key);
+        // console.log("valeurs", values);
+        // console.log("latitude",values.lat);
+        // console.log("longitude",values.lon);
+        // console.log("categorie",values.cat);
         //Récupération des valeurs correspondant aux clés, lat,lon et cat
         var latitude = values.lat;
         var longitude = values.lon;
         var categorie = values.cat;
-        //Ajout du point sur la carte
-        markers = L.marker([latitude, longitude]).addTo(map).bindPopup("<b>"+categorie+"</b>").openPopup();
+
+        //Ajout du point sur la carte avec la couleur de la catégorie
+        switch (categorie) {
+
+            case 'Dégradations urbaines':
+                markers = L.marker([latitude, longitude], {
+                    icon: markerBleuF
+                }).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
+                break;
+
+            case 'Propreté de la ville':
+                markers = L.marker([latitude, longitude], {
+                    icon: markerBleu
+                }).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
+                break;
+
+            case 'Voierie / Chaussée':
+                markers = L.marker([latitude, longitude], {
+                    icon: markerRouge
+                }).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
+                break;
+
+            case 'Eclairage public':
+                markers = L.marker([latitude, longitude], {
+                    icon: markerOrange
+                }).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
+                break;
+
+            case 'Animaux perdus / errants':
+                markers = L.marker([latitude, longitude], {
+                    icon: markerViolet
+                }).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
+                break;
+
+            case 'Espaces verts':
+                markers = L.marker([latitude, longitude], {
+                    icon: markerVert
+                }).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
+                break;
+
+            case 'Encombrants':
+                markers = L.marker([latitude, longitude], {
+                    icon: markerGris
+                }).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
+                break;
+
+            case 'Autre':
+                markers = L.marker([latitude, longitude], {
+                    icon: markerNoir
+                }).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
+                break;
+
+            default:
+                markers = L.marker([latitude, longitude]).addTo(map).bindPopup("<b>" + categorie + "</b>").openPopup();
         }
+
+    }
 }
 
 createMarkers();
 
-    var lat = 43.116312;
-    var lon = 1.61;
+var lat = 43.116312;
+var lon = 1.61;
 
-    console.log(lat);
-    console.log(lon);
+console.log(lat);
+console.log(lon);
 
-    sessionStorage.setItem("lat",lat);
-    sessionStorage.setItem("lon",lon);
-    
-});
+sessionStorage.setItem("lat", lat);
+sessionStorage.setItem("lon", lon);
+
+// });
